6a963007fd83f09603a94875b8de2f819364a49f
parkikbum <pibum@naver.com>
2021-06-07 23:57:34 +0900

    [ADD]수정중

--- Awesome/Awesome/Cells/NoScheduleTableViewCell.swift
+++ Awesome/Awesome/Cells/NoScheduleTableViewCell.swift
@@ -16,6 +16,10 @@ class NoScheduleTableViewCell: UITableViewCell {
         // Initialization code
     }
 
+    
+    
+    
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
