780f54482f0804b5e9549d5aa2cc1c5d5bf54f9a
parkikbum <pibum@naver.com>
2021-07-20 08:03:44 +0900

    [ADD]자동로그인 서비스 파일

--- Awesome/Awesome/Network/Service/AutoLoginService.swift
+++ Awesome/Awesome/Network/Service/AutoLoginService.swift
@@ -1,93 +1,68 @@
 import Foundation
 import Alamofire
 
-struct AutoLoginService {
+struct GetAutoLoginService {
+    static let shared = GetAutoLoginService()
     
-    static let shared = AutoLoginService()
-    let header : HTTPHeaders = ["Content-Type" : "application/json"]
-    
-    
-    //userId, region, theme, warning
-    private func makeParameter(refreshToken: String) -> Parameters {
-        return  [ "refreshToken" : refreshToken]
+    private func makeParameter(refresh_token : String) -> Parameters
+    {
+        return ["refresh_token": refresh_token]
     }
     
-    func postSearchKeywords(refreshToken: String,
-                            completion: @escaping (NetworkResult<Any>) -> Void){
-        
-        let dataRequest = AF.request(Constants.updateLoginToken+type,
+    func AutoLoginService(refresh_token : String,
+              completion : @escaping (NetworkResult<Any>) -> Void)
+    {
+        let header : HTTPHeaders = ["Content-Type": "application/json"]
+        let dataRequest = AF.request(Constants.updateLoginToken,
                                      method: .post,
-                                     parameters: makeParameter(refreshToken: refreshToken),
+                                     parameters: makeParameter(refresh_token: refresh_token),
                                      encoding: JSONEncoding.default,
                                      headers: header)
         
-        dataRequest.responseData{ dataResponse in
-            switch dataResponse.result{
+        
+        dataRequest.responseData { dataResponse in
+            
+            switch dataResponse.result {
             case .success:
-                guard let statusCode = dataResponse.response?.statusCode else { return}
-                guard let value = dataResponse.value  else {return}
+                
+                print("----- 데이터 요청 성공")
+                guard let statusCode = dataResponse.response?.statusCode else {return}
+                print(dataRequest.response?.statusCode)
+                guard let value = dataResponse.value else {return}
                 let networkResult = self.judgeStatus(by: statusCode, value)
                 completion(networkResult)
                 
-            case .failure(_): completion(.pathErr)
-            }
-        }
-    }
-    
-    func getPostToken(refreshToken : String,
-                        completion: @escaping (NetworkResult<Any>) -> Void){
-       
-        
-        
-        let dataRequeat = AF.download(Constants.updateLoginToken,
-                                      method: .get,
-                                      encoding: JSONEncoding.default,
-                                      headers: header)
-        
-        dataRequeat.responseData{ dataResponse in
-            switch dataResponse.result{
-            case .success:
-                guard let statusCode = dataResponse.response?.statusCode else { return}
-                guard let value = dataResponse.value  else {return}
-                let networkResult = self.judgeStatusPostDetail(by: statusCode, value)
-                completion(networkResult)
+            case .failure: completion(.pathErr)
                 
-            case .failure(_): completion(.pathErr)
             }
         }
         
     }
     
     private func judgeStatus(by statusCode: Int, _ data: Data) -> NetworkResult<Any> {
+        
         let decoder = JSONDecoder()
         
-        guard let decodeData = try? decoder.decode(DetailModel.self, from: data) else {
+        
+        guard let decodedData = try? decoder.decode(AutoLoginDataModel.self, from: data)
+        else {
+            print("패쓰에러")
             return .pathErr
+            
         }
         
         switch statusCode {
-        case 200: return .success(decodeData)
-        case 400: return .requestErr(decodeData)
+        
+        case 200:
+            print("--- 데이터 받기 성공")
+            let usertoken = UserDefaults.standard.set(decodedData.accesstoken, forKey: "accessToken")
+            print("dd", UserDefaults.standard.string(forKey: "accessToken"))
+            return .success(decodedData.accesstoken)
+        case 400: return .requestErr(decodedData)
         case 500: return .serverErr
-        default:
-            return .networkFail
+        default: return .networkFail
         }
     }
     
-    private func judgeStatusPostDetail(by statusCode: Int, _ data: Data) -> NetworkResult<Any> {
-        let decoder = JSONDecoder()
-        
-        guard let decodeData = try? decoder.decode(AutoLoginDataModel.self, from: data) else {
-            return .pathErr
-        }
-
-        switch statusCode {
-        case 200: return .success(decodeData)
-            print(decodeData.accesstoken)
-        case 400: return .requestErr(decodeData)
-        case 500: return .serverErr
-        default:
-            return .networkFail
-        }
-    }
+    
 }
