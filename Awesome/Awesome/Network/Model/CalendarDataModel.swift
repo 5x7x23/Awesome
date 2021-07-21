c399d2bdeec6e2f3dff6fab91cb584a80744a0f2
parkikbum <pibum@naver.com>
2021-07-21 23:28:29 +0900

    [ADD] 캘린더 데이터 모델 생성

--- Awesome/Awesome/Network/Model/CalendarDataModel.swift
+++ Awesome/Awesome/Network/Model/CalendarDataModel.swift
@@ -1,17 +1,18 @@
+
 import Foundation
 
 // MARK: - CalendarDataModel
 struct CalendarDataModel: Codable {
-    let createdCalendar: [String]
-    let myCalendar: [MyCalendar]
+    let createdCalendar, myCalendar, calendar: [MyCalendar]
 
     enum CodingKeys: String, CodingKey {
         case createdCalendar = "created_calendar"
         case myCalendar = "my_calendar"
+        case calendar
     }
 }
 
-// MARK: - MyCalendar
+// MARK: - Calendar
 struct MyCalendar: Codable {
     let id, creator, participant: Int
     let comment: String
@@ -23,3 +24,4 @@ struct MyCalendar: Codable {
         case endDate = "end_date"
     }
 }
+
