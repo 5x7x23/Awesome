a566a37ee3c45cea207f9f22e65af8b58a9dc3c2
parkikbum <pibum@naver.com>
2021-07-25 22:55:09 +0900

    [CHORE] 카카오 데이터 모델 수정사항 반영

--- Awesome/Awesome/Network/Model/KakaoLoginDataModel.swift
+++ Awesome/Awesome/Network/Model/KakaoLoginDataModel.swift
@@ -12,11 +12,20 @@ struct KakaoLoginDataModel: Codable {
     let code: String
 }
 
+
+
 struct KakaoLoginTokenModel: Codable {
     let accessToken, refreshToken: String
+    let user: User
+
 
     enum CodingKeys: String, CodingKey {
+        case user
         case accessToken = "access_token"
         case refreshToken = "refresh_token"
     }
+}
+struct User: Codable {
+    let id: Int
+    let name: String
 }
