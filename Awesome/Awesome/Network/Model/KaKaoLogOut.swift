c0cc89b16d67ef1c70ad0d87f3c3d48ebc948f49
parkikbum <pibum@naver.com>
2021-07-25 22:54:31 +0900

    [FIX] 로그아웃 안되던 부분 수정

--- Awesome/Awesome/Network/Model/KaKaoLogOut.swift
+++ Awesome/Awesome/Network/Model/KaKaoLogOut.swift
@@ -2,17 +2,8 @@ import Foundation
 
 // MARK: - LogoutDataModel
 struct LogoutDataModel: Codable {
-    let detail, code: String
-    let messages: [Message]
+    let detail: String
 }
 
-// MARK: - Message
-struct Message: Codable {
-    let tokenClass, tokenType, message: String
 
-    enum CodingKeys: String, CodingKey {
-        case tokenClass = "token_class"
-        case tokenType = "token_type"
-        case message
-    }
-}
+
