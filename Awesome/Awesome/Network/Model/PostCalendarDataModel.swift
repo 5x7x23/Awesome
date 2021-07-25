5702631b4676378fa7239884438f62c9182150d4
parkikbum <pibum@naver.com>
2021-07-25 22:55:37 +0900

    [ADD] 내 일정 추가 데이터 모델  반영

--- Awesome/Awesome/Network/Model/PostCalendarDataModel.swift
+++ Awesome/Awesome/Network/Model/PostCalendarDataModel.swift
@@ -1,8 +1,29 @@
-//
-//  PostCalendarDataModel.swift
-//  Awesome
-//
-//  Created by 박익범 on 2021/07/20.
-//
 
 import Foundation
+
+// MARK: - LogoutDataModel
+struct PostScheduleDataModel: Codable {
+    let status: String
+    let data: scheduleModel
+}
+
+// MARK: - DataClass
+struct scheduleModel: Codable {
+    let id, creator: Int
+       let creatorName: String
+       let participant: Int
+       let participantName, comment: String
+       let startDate, endDate: Date
+       let createdAt: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id, creator
+                case creatorName = "creator_name"
+                case participant
+                case participantName = "participant_name"
+                case comment
+                case startDate = "start_date"
+                case endDate = "end_date"
+                case createdAt = "created_at"
+    }
+}
